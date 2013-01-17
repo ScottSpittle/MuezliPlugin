7c152c161c18b902f081436772ecc394be8a7faa
Scott <Scott@Scott-PC>
2013-01-17 21:00:09 +0000

    Updated License information on each class.

--- src/me/ScottSpittle/MuezliPlugin/MySQL.java
+++ src/me/ScottSpittle/MuezliPlugin/MySQL.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2013 Scott Spittle, James Loyd
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package me.ScottSpittle.MuezliPlugin;
 
 
@@ -38,7 +53,7 @@ public class MySQL {
     	}catch (SQLException s){
     		System.out.println("SQL statement is not executed!");
     	}finally {
-    		conn = null;
+    		if(conn != null) try {conn.close();} catch(SQLException ignore){};
     	}
     }
 
@@ -64,7 +79,7 @@ public class MySQL {
     	}catch (SQLException s){
     		System.out.println("SQL statement is not executed!");
     	}finally {
-    		conn = null;
+    		if(conn != null) try {conn.close();} catch(SQLException ignore){};
     	}
     }
 } 
