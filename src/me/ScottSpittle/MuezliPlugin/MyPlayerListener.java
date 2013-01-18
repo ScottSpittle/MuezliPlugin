a4fd913b133a700c737e71e1a9b5e8a6dec756f6
Scott <Scott@Scott-PC>
2013-01-18 23:56:42 +0000

    Again, redone entire class to optimize it, left it open for some crazy shit to be done. when we decide what... xD

--- src/me/ScottSpittle/MuezliPlugin/MyPlayerListener.java
+++ src/me/ScottSpittle/MuezliPlugin/MyPlayerListener.java
@@ -17,54 +17,29 @@ package me.ScottSpittle.MuezliPlugin;
 
 import net.milkbowl.vault.permission.Permission;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MyPlayerListener implements Listener {
 	
-	public static Main plugin;
     public static Permission perms = null;
 		
 	@EventHandler
-	public void onPlayerJoin (PlayerJoinEvent event){
-		Player player = event.getPlayer();
-		event.setJoinMessage(ChatColor.BLUE + "Hey " + player.getName() + ", Welcome to the Muezli-Server");
+	public void onPlayerJoin (PlayerJoinEvent e){
+		Main.plugin.getServer().broadcastMessage("Welcome " + e.getPlayer().getName() + " To The Server");
+	}
+	@EventHandler
+	public void onPlayerQuit (PlayerQuitEvent e){
+		//Player player = event.getPlayer();
+		Main.plugin.getServer().broadcastMessage(e.getPlayer().getName() + "Fucked off..");
 	}
 
 	@EventHandler
-	public void onPlayerInteract (PlayerInteractEvent event){
-		Player player = event.getPlayer();
-		if(player.isOp()){
-			int blockID = player.getItemInHand().getType().getId();
-
-			if(blockID == 1){
-				if(perms.has(player, "muezli.explode")){
-					Block block = player.getTargetBlock(null, 50);
-					Location location = block.getLocation();
-					World world = player.getWorld();
-					world.createExplosion(location, 5);
-					player.sendMessage(ChatColor.BLUE + "willies");
-				}else{
-					player.sendMessage(ChatColor.DARK_RED + "No Permissions");
-				}
-			}else if(blockID == 2){
-				if(perms.has(player, "muezli.lightning")){
-					Block block = player.getTargetBlock(null, 50);
-					Location location = block.getLocation();
-					World world = player.getWorld();
-					world.strikeLightning(location);
-				}else{
-					player.sendMessage(ChatColor.DARK_RED + "No Permissions");
-				}
-			}
-		}
+	public void onPlayerChat (AsyncPlayerChatEvent e){
 		
 	}
 }
