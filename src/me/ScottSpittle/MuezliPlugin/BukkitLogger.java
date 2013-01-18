1079982d4f7149617053e838070dc826a3c92785
Scott <Scott@Scott-PC>
2013-01-18 23:54:50 +0000

    Restarted class from Scratch, optimized it etc..

--- src/me/ScottSpittle/MuezliPlugin/BukkitLogger.java
+++ src/me/ScottSpittle/MuezliPlugin/BukkitLogger.java
@@ -17,25 +17,21 @@ package me.ScottSpittle.MuezliPlugin;
 
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 
 public class BukkitLogger {
 
 	public static Main plugin;
 	
-	public BukkitLogger(Main instance){
-		plugin = instance;
-
-	}
-	
 	public final Logger logger = Logger.getLogger("Minecraft");
 	
 	public void enabled(boolean enabled){
 		PluginDescriptionFile pdfFile = plugin.getDescription();
 		if(enabled){
-			this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Enabled");
+			plugin.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Enabled");
 		}else {
-			this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Disabled");
+			plugin.getServer().getConsoleSender().sendMessage(ChatColor.RED + pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has been Disabled");
 		}
 	}
 }
